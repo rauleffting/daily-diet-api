4341a700440e4cf638eb7113283932d9f19688cf
Raul Effting <rauleffting94@gmail.com>
2023-08-03 23:31:14 -0300

    chore: set up knex and database configuration

---
+++ src/database.ts
@@ -0,0 +1,13 @@
+import { knex as setupKnex, type Knex } from 'knex'
+
+export const config: Knex.Config = {
+  client: 'sqlite',
+  connection: './db/app.db',
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations'
+  }
+}
+
+export const knex = setupKnex(config)
