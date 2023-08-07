5c7141377d00ea2b86cd9decb90fee54fba28225
Raul Effting <rauleffting94@gmail.com>
2023-08-07 17:08:56 -0300

    chore: update database filename to use env variable in database.ts

--- src/database.ts
+++ src/database.ts
@@ -4,7 +4,7 @@ import { env } from './env'
 export const config: Knex.Config = {
   client: env.DATABASE_CLIENT,
   connection: {
-    filename: './db/app.db'
+    filename: env.DATABASE_URL
   },
   useNullAsDefault: true,
   migrations: {
