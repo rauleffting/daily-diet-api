b4e3cb33a7da4f6cac7314cf8d841a2d78af56f5
Raul Effting <rauleffting94@gmail.com>
2023-08-06 23:53:46 -0300

    refactor: update users migration to have email and password

--- db/migrations/20230805025040_create-users.ts
+++ db/migrations/20230805025040_create-users.ts
@@ -3,7 +3,8 @@ import { type Knex } from 'knex'
 export async function up (knex: Knex): Promise<void> {
   await knex.schema.createTable('users', (table) => {
     table.uuid('id').primary()
-    table.text('name').notNullable()
+    table.text('email').notNullable()
+    table.text('password').notNullable()
   })
 }
 
