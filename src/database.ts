8c6668de30d3093165fe50a4f8d0dcbeaf926aa8
Raul Effting <rauleffting94@gmail.com>
2023-08-07 17:00:15 -0300

    chore: update database.ts to receive .env database_client

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,8 @@
 import { knex as setupKnex, type Knex } from 'knex'
+import { env } from './env'
 
 export const config: Knex.Config = {
-  client: 'sqlite',
+  client: env.DATABASE_CLIENT,
   connection: './db/app.db',
   useNullAsDefault: true,
   migrations: {
