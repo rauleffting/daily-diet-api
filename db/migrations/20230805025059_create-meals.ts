ce75e4b51979079f764815823d8fbcfc6fed3df6
Raul Effting <rauleffting94@gmail.com>
2023-08-07 13:29:06 -0300

    chore: update knex migrations

--- db/migrations/20230805025059_create-meals.ts
+++ db/migrations/20230805025059_create-meals.ts
@@ -3,7 +3,6 @@ import { type Knex } from 'knex'
 export async function up (knex: Knex): Promise<void> {
   await knex.schema.createTable('meals', (table) => {
     table.uuid('id').primary()
-    table.uuid('user_id').references('id').inTable('users').onDelete('CASCADE')
     table.text('name').notNullable()
     table.text('description').notNullable()
     table.boolean('is_diet_meal').defaultTo(false)
