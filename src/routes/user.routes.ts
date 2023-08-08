f56cdd00c7fc56feb908325f499ec2680ae1ee6a
Raul Effting <rauleffting94@gmail.com>
2023-08-07 23:47:16 -0300

    feat: add post /login endpoint

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,4 +1,4 @@
-import { hash } from 'bcrypt'
+import { compare, hash } from 'bcrypt'
 import { randomUUID } from 'crypto'
 import { type FastifyInstance } from 'fastify'
 import { knex } from '../database'
@@ -10,7 +10,7 @@ export async function userRoutes (app: FastifyInstance): Promise<void> {
       const { email, password } = createUserBodySchema.parse(request.body)
 
       const userExists = await knex('users')
-        .where('email', email)
+        .where({ email })
         .first()
 
       if (userExists != null) {
@@ -29,6 +29,42 @@ export async function userRoutes (app: FastifyInstance): Promise<void> {
     } catch (error) {
       console.error(error) // Log the error for debugging purposes
       return await reply.status(500).send({ message: 'Error creating user!' })
+    }
+  })
+
+  app.post('/login', async (request, reply) => {
+    try {
+      const { email, password } = createUserBodySchema.parse(request.body)
+
+      const user = await knex('users')
+        .where({ email })
+        .first()
+
+      if (user === null) {
+        return await reply.status(509).send({ message: 'E-mail e/ou senha incorretos.' })
+      }
+
+      const passwordMatched = await compare(password, user.password)
+
+      if (!passwordMatched) {
+        return await reply.status(509).send({ message: 'E-mail e/ou senha incorretos.' })
+      }
+
+      let sessionId = request.cookies.sessionId
+
+      if (sessionId === null) {
+        sessionId = randomUUID()
+
+        reply.cookie('sessionId', sessionId, {
+          path: '/',
+          maxAge: 1000 * 60 * 60 * 24 * 30 * 12 // 1 year
+        })
+      }
+
+      return await reply.status(201).send({ message: 'User successfully logged!' })
+    } catch (error) {
+      console.error(error) // Log the error for debugging purposes
+      return await reply.status(509).send({ message: 'Error trying to log in!' })
     }
   })
 }
