4c9884e9c0421f63cf1d7e5594aeca315c28bb5f
Raul Effting <rauleffting94@gmail.com>
2023-08-07 23:46:22 -0300

    feat: register cookie

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 import { userRoutes } from './routes/user.routes'
 
 export const app = fastify()
 
+app.register(cookie)
 app.register(userRoutes)
