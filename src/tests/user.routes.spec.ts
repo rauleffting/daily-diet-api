0fda9a9259953dd915c72a2b02375d34fef957eb
Raul Effting <rauleffting94@gmail.com>
2023-08-08 14:06:11 -0300

    test: ensure user can log in

---
+++ src/tests/user.routes.spec.ts
@@ -0,0 +1,51 @@
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
+import { execSync } from 'node:child_process'
+import request from 'supertest'
+import { app } from '../app'
+import { knex } from '../database'
+
+describe('User routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
+  it('should create a new user', async () => {
+    const mockUser = {
+      email: 'test@example.com',
+      password: 'test123'
+    }
+
+    const response = await request(app.server)
+      .post('/register')
+      .send(mockUser)
+
+    expect(response.statusCode).toBe(201)
+    expect(response.body.message).toBe('User successfully created!')
+
+    const user = await knex('users').where('email', 'test@example.com').first()
+    expect(user).not.toBeNull()
+  })
+
+  it('should allow a user to log in with a valid email and password', async () => {
+    const mockUser = {
+      email: 'test@example.com',
+      password: 'test123'
+    }
+
+    await request(app.server).post('/register').send(mockUser)
+
+    const response = await request(app.server).post('/login').send(mockUser)
+
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual({ message: 'User successfully logged!' })
+  })
+})
