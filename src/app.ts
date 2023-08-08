5739a3bff9afe06d32b817a5ec0619f5679f0723
Raul Effting <rauleffting94@gmail.com>
2023-08-08 18:36:28 -0300

    feat: add registration of meal routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
 import { userRoutes } from './routes/user.routes'
+import { mealRoutes } from './routes/meal.routes'
 
 export const app = fastify()
 
 app.register(cookie)
+app.register(mealRoutes)
 app.register(userRoutes)
