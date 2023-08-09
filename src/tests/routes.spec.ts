477a1746c7bcfe6e3656e70e624e50baae3ff23d
Raul Effting <rauleffting94@gmail.com>
2023-08-09 16:47:13 -0300

    test: ensure user can delete meal by id

--- src/tests/routes.spec.ts
+++ src/tests/routes.spec.ts
@@ -204,4 +204,39 @@ describe('Meal routes', () => {
     expect(response.statusCode).toBe(200)
     expect(response.body).toBeDefined()
   })
+
+  it('should delete meal by id', async () => {
+    const mockUser = {
+      email: 'test@example.com',
+      password: 'test123'
+    }
+
+    const mockMeal = {
+      name: 'McDonalds',
+      description: 'Big Tasy + Milk Shake',
+      isDietMeal: false
+    }
+
+    await request(app.server).post('/register').send(mockUser)
+    const logIn = await request(app.server).post('/login').send(mockUser)
+
+    const cookies = logIn.get('Set-Cookie')
+
+    await request(app.server)
+      .post('/meal')
+      .set('Cookie', cookies)
+      .send(mockMeal)
+
+    const meals = await request(app.server)
+      .get('/meal')
+      .set('Cookie', cookies)
+
+    const firstMealId: string = meals.body[0].id
+
+    const response = await request(app.server)
+      .delete(`/meal/${firstMealId}`)
+      .set('Cookie', cookies)
+
+    expect(response.statusCode).toBe(200)
+  })
 })
